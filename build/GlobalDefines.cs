a3e96136951cf5bab91449562400432643511acd
Antonius Riha <antoniusriha@gmail.com>
2012-06-15 12:03:11 +0200

    Change GlobalDefines.cs build
    GlobalDefines.cs holds information like version number and license about Tasque. It is generated by a special MSBuild task ("SetDataAtBuildTime") from GlobalDefines.cs.in after the build of build.csproj (which builds the SetDataAtBuildTime MSBuild task). GlobalDefines.cs is then linked to by the various .csproj files of the solution which include it in their build.
    
    * build.mdproj: has been renamed to build.csproj
    * build.csproj:
    	- removed all stuff related to MSBuild.Community.Tasks, which is not needed anymore
    	- added build configuration to build SetDataAtBuildTime.cs
    
    * tasque.sln:
    	- reflect build proj changes
    
    * .gitignore:
    	- reflect removal of MSBuild.Community.Tasks
    	- add ignore of .mdb files
    
    * MSBuild.Community.Tasks.dll and MSBuild.Community.Tasks.Targets: Those are not needed anymore, since everything is now handled by a custom self-written task
    
    * GlobalDefines.cs.in and GlobalDefines.cs:
    	- include some more data about the project

--- build/GlobalDefines.cs
+++ build/GlobalDefines.cs
@@ -1,22 +1,22 @@
 // This file (GlobalDefines.cs) is automatically generated. Do not edit. (Edit GlobalDefines.cs.in instead.)
-//
+// 
 // GlobalDefines.cs
-//
+//  
 // Author:
 //       Antonius Riha <antoniusriha@gmail.com>
-//
+// 
 // Copyright (c) 2012 Antonius Riha
-//
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,11 +24,39 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
+using System.Collections.ObjectModel;
+
 namespace Tasque {
-static class GlobalDefines {
-public const string Version = "0.1.10";
-public const string DataDir	= "@datadir@";
-public const string LocaleDir = "@datadir@/locale";
-public const string SoundDir = "@datadir@/tasque/sounds";
-}
+	static class GlobalDefines {
+		public const string Version = "0.1.10";
+//		public const string DataDir	= "@datadir@";
+//		public const string LocaleDir = "@datadir@/locale";
+//		public const string SoundDir = "@datadir@/tasque/sounds";
+		public const string CopyrightInfo = @"Copyright (c) 2008 Novell, Inc.
+";
+		public const string License = @"Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+""Software""), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+";
+		public const string Website = "https://live.gnome.org/Tasque";
+		public static readonly ReadOnlyCollection<string> Authors =
+			new ReadOnlyCollection<string> (new Collection<string> () { "Boyd Timothy <btimothy@gmail.com>", "Calvin Gaisford <calvinrg@gmail.com>", "Brian Merrell <bgmerrell@gmail.com>", "Sandy Armstrong <sanfordarmstrong@gmail.com>",  });
+	}
 }
---
+++ build/SetDataAtBuildTime.cs
@@ -0,0 +1,211 @@
+//
+// SetDataAtBuildTime.cs
+//
+// Author:
+//       Antonius Riha <antoniusriha@gmail.com>
+//
+// Copyright (c) 2012 Antonius Riha
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Tasque.Build
+{
+	public class SetDataAtBuildTime : Task
+	{
+		[Required]
+		public ITaskItem AuthorsFile {
+			get {
+				return authorsFile;
+			}
+			set {
+				authorsFile = value;
+
+				try {
+					using (var fs = new FileStream (value.GetMetadata ("FullPath"), FileMode.Open)) {
+						using (var sr = new StreamReader (fs)) {
+
+							var authorsStrBuilder = new StringBuilder ();
+
+							while (!sr.EndOfStream) {
+								var line = sr.ReadLine ().Trim ();
+								if (!string.IsNullOrWhiteSpace (line))
+									authorsStrBuilder.Append ("\"" + line + "\", ");
+							}
+
+							authors = authorsStrBuilder.ToString ();
+						}
+					} 
+				} catch (Exception ex) {
+					Log.LogErrorFromException (ex);
+				}
+			}
+		}
+
+		[Required]
+		public ITaskItem CopyingFile {
+			get {
+				return copyingFile;
+			}
+			set {
+				copyingFile = value;
+
+				try {
+					using (var fs = new FileStream (value.GetMetadata ("FullPath"), FileMode.Open)) {
+						using (var sr = new StreamReader (fs)) {
+
+							/*
+							 * Parsing states:
+							 * 0 ... nothing parsed yet
+							 * 1 ... parsing copyright notice
+							 * 2 ... before parsing license text
+							 * 3 ... parsing license text
+							 * 
+							 * The text format to be parsed is:
+							 * [empty line]*
+							 * [paragraph with copyright notices]
+							 * [empty line]*
+							 * [paragraphs with license text]
+							 */
+							int state = 0;
+
+							var copyrightStrBuilder = new StringBuilder ();
+							var licenseStrBuilder = new StringBuilder ();
+
+							while (!sr.EndOfStream) {
+								var line = sr.ReadLine ().Trim ();
+								switch (state) {
+								case 0:
+									if (string.IsNullOrWhiteSpace (line))
+										break;
+									else {
+										state = 1;
+										goto case 1;
+									}
+								case 1:
+									if (!string.IsNullOrWhiteSpace (line)) {
+										copyrightStrBuilder.AppendLine (line);
+										break;
+									} else {
+										state = 2;
+										break;
+									}
+								case 2:
+									if (string.IsNullOrWhiteSpace (line))
+										break;
+									else {
+										state = 3;
+										goto case 3;
+									}
+								case 3:
+									licenseStrBuilder.AppendLine (line);
+									break;
+								}
+							}
+
+							/* 
+							 * Replace double quotes with double double quotes, which is
+							 * necessary to escape double quotes in verbatim C# string constants.							 * 
+							 */
+							copyrightStrBuilder.Replace ("\"", "\"\"");
+							licenseStrBuilder.Replace ("\"", "\"\"");
+
+							copyrightInfo = copyrightStrBuilder.ToString ();
+							license = licenseStrBuilder.ToString ();
+						}
+					}
+				} catch (Exception ex) {
+					Log.LogErrorFromException (ex);
+				}
+			}
+		}
+
+		[Required]
+		public ITaskItem GlobalDefinesFile { get; set; }
+
+		[Required]
+		public ITaskItem GlobalDefinesFileIn { get; set; }
+
+		[Required]
+		public ITaskItem Version { get; set; }
+
+		[Required]
+		public ITaskItem Website { get; set; }
+
+		public override bool Execute ()
+		{
+			if (!ReadGlobalDefines ())
+				return false;
+
+			SetValue ("@version@", Version.ItemSpec);
+
+			SetValue ("@copyrightinfo@", copyrightInfo);
+
+			SetValue ("@license@", license);
+
+			SetValue ("@website@", Website.ItemSpec);
+
+			SetValue ("@authors@", authors);
+
+			if (!WriteGlobalDefines ())
+				return false;
+
+			return true;
+		}
+
+		bool ReadGlobalDefines ()
+		{
+			try {
+				globalDefines = File.ReadAllText (GlobalDefinesFileIn.GetMetadata ("FullPath"));
+			} catch (Exception ex) {
+				Log.LogErrorFromException (ex);
+				return false;
+			}
+			return true;
+		}
+
+		void SetValue (string key, string value)
+		{
+			globalDefines = globalDefines.Replace (key, value);
+		}
+
+		bool WriteGlobalDefines ()
+		{
+			try {
+				File.WriteAllText (GlobalDefinesFile.GetMetadata ("FullPath"), globalDefines);
+			} catch (Exception ex) {
+				Log.LogErrorFromException (ex);
+				return false;
+			}
+			return true;
+		}
+
+		ITaskItem authorsFile;
+		string authors;
+		ITaskItem copyingFile;
+		string copyrightInfo;
+		string globalDefines;
+		string license;
+	}
+}
