19466a6a4bc6e6b77276f30b2d078b021f9a5220
Antonius Riha <antoniusriha@gmail.com>
2012-07-13 15:47:41 +0200

    Initial ViewModel support

--- src/libtasque/Task.cs
+++ src/libtasque/Task.cs
@@ -24,8 +24,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
-using System.ComponentModel;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace Tasque
 {
--- src/libtasqueui/Properties/AssemblyInfo.cs
+++ src/libtasqueui/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("libtasque")]
+[assembly: AssemblyTitle("libtasqueui")]
 [assembly: AssemblyCopyright("MIT")]
