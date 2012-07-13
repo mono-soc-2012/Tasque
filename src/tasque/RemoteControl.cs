mono-soc-2012/Tasque

commit 236ee81e81f013fd89972d4e6e5ad58bdc45ab08

Change Categories type from Gtk.TreeModel to ObservableCollection<T>.
* ICategory.cs: must be INotifyPropertyChanged in order to work with CollectionView.

+++
NOTE: This should not be necessary and must be fixed in CollectionView, preferably by adopting the .NET implementation of CollectionView/ListCollectionView/...
Issue has been filed on Github.
+++

src/tasque/RemoteControl.cs
@@ -85,37 +85,23 @@ namespace Tasque
 		public string CreateTask (string categoryName, string taskName,
 						bool enterEditMode, bool parseDate)
 		{
-			Gtk.TreeIter iter;
-			Gtk.TreeModel model = Application.Backend.Categories;
+			var categories = Application.Backend.Categories;
 			
 			//
 			// Validate the input parameters.  Don't allow null or empty strings
 			// be passed-in.
 			//
-			if (categoryName == null || categoryName.Trim () == string.Empty
-					|| taskName == null || taskName.Trim () == string.Empty) {
+			if (string.IsNullOrWhiteSpace (categoryName) || string.IsNullOrWhiteSpace (taskName))
 				return string.Empty;
-			}
 			
 			//
 			// Look for the specified category
 			//
-			if (!model.GetIterFirst (out iter)) {
-				return string.Empty;
-			}
-			
 			ICategory category = null;
-			do {
-				ICategory tempCategory = model.GetValue (iter, 0) as ICategory;
-				if (tempCategory.Name.ToLower ().CompareTo (categoryName.ToLower ()) == 0) {
-					// Found a match
-					category = tempCategory;
-				}
-			} while (model.IterNext (ref iter));
+			categories.SingleOrDefault (c => c.Name.ToLower () == categoryName.ToLower ());
 			
-			if (category == null) {
+			if (category == null)
 				return string.Empty;
-			}
 			
 			// If enabled, attempt to parse due date information
 			// out of the taskName.
@@ -167,20 +153,14 @@ namespace Tasque
 		public string[] GetCategoryNames ()
 		{
 			List<string> categories = new List<string> ();
-			string[] emptyArray = categories.ToArray ();
+			var model = Application.Backend.SortedCategories;
 			
-			Gtk.TreeIter iter;
-			Gtk.TreeModel model = Application.Backend.Categories;
-			
-			if (!model.GetIterFirst (out iter))
-				return emptyArray;
-			
-			do {
-				ICategory category = model.GetValue (iter, 0) as ICategory;
-				if (category is AllCategory)
-					continue; // Prevent the AllCategory from being returned
-				categories.Add (category.Name);
-			} while (model.IterNext (ref iter));
+			foreach (var item in model) {
+				if (item is AllCategory)
+					continue;
+				
+				categories.Add (((ICategory)item).Name);
+			}
 			
 			return categories.ToArray ();
 		}
@@ -280,29 +260,14 @@ namespace Tasque
 		/// A <see cref="System.Boolean"/>, true for success, false
 		/// for failure.
 		/// </returns>
-		public bool SetCategoryForTaskById (string id,
-							string categoryName)
+		public bool SetCategoryForTaskById (string id, string categoryName)
 		{
 			ITask task = GetTaskById (id);
 			if (task == null)
-			{
-				return false;
-			}
-			Gtk.TreeIter iter;
-			Gtk.TreeModel model = Application.Backend.Categories;
-			
-			if (!model.GetIterFirst (out iter))
 				return false;
 			
-			do {
-				ICategory category = model.GetValue (iter, 0) as ICategory;
-				if (string.Compare(category.Name,categoryName)==0)
-				{
-					task.Category = category;
-					return true;
-				}
-			} while (model.IterNext (ref iter));
-			return false;
+			var categories = Application.Backend.Categories;
+			return categories.Contains (task.Category);
 		}
 		
 		/// <summary>

src/libtasque/IBackend.cs
@@ -46,9 +46,9 @@ namespace Tasque.Backends
 		/// <value>
 		/// This returns all the ICategory items from the backend.
 		/// </value>
-		Gtk.TreeModel Categories {
-			get;
-		}
+		ObservableCollection<ICategory> Categories { get; }
+		
+		IEnumerable SortedCategories { get;	}
 		
 		/// <value>
 		/// Indication that the backend has enough information

src/libtasque/ICategory.cs
@@ -3,17 +3,39 @@
 //
 // To change standard headers go to Edit->Preferences->Coding->Standard Headers
 //
-
+// 
+// ICategory.cs
+//  
+// Author:
+//       Antonius Riha <antoniusriha@gmail.com>
+// 
+// Copyright (c) 2012 Antonius Riha
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 using System;
+using System.ComponentModel;
 
 namespace Tasque
 {
-	public interface ICategory
+	public interface ICategory : IComparable<ICategory>, INotifyPropertyChanged
 	{
-		string Name
-		{
-			get;
-		}
+		string Name	{ get; }
 		
 		bool ContainsTask(ITask task);
 	}

src/tasque/AllCategory.cs
@@ -50,5 +50,16 @@ namespace Tasque
 			categoriesToHide =
 				preferences.GetStringList (Preferences.HideInAllCategory);
 		}
+
+		#region IComparable implementation
+		public int CompareTo (ICategory other)
+		{
+			return -1;
+		}
+		#endregion
+
+		#region INotifyPropertyChanged implementation
+		public event PropertyChangedEventHandler PropertyChanged;
+		#endregion
 	}
 }

src/tasque/Backends/Dummy/DummyBackend.cs
@@ -19,12 +19,9 @@ namespace Tasque.Backends.Dummy
 		/// Key   = Task ID
 		/// Value = Gtk.TreeIter in taskStore
 		/// </summary>
-		private int newTaskId;
-		private bool initialized;
-		private bool configured = true;
-		
-		private Gtk.ListStore categoryListStore;
-		private Gtk.TreeModelSort sortedCategoriesModel;
+		int newTaskId;
+		bool initialized;
+		bool configured = true;
 
 		public event BackendInitializedHandler BackendInitialized;
 		public event BackendSyncStartedHandler BackendSyncStarted;
@@ -39,19 +36,18 @@ namespace Tasque.Backends.Dummy
 			initialized = false;
 			newTaskId = 0;
 			Tasks = new ObservableCollection<ITask> ();
-			var cv =  new CollectionView<ITask> (Tasks);
+			var cvTasks =  new CollectionView<ITask> (Tasks);
 			/*
 			 * this invokes the default comparer, which in turn
 			 * will use the IComparable implmentation of Task
 			 */
-			cv.SortDescriptions.Add (new SortDescription ());
-			SortedTasks = cv;
+			cvTasks.SortDescriptions.Add (new SortDescription ());
+			SortedTasks = cvTasks;
 			
-			categoryListStore = new Gtk.ListStore (typeof(ICategory));
-			
-			sortedCategoriesModel = new Gtk.TreeModelSort (categoryListStore);
-			sortedCategoriesModel.SetSortFunc (0, new Gtk.TreeIterCompareFunc (CompareCategorySortFunc));
-			sortedCategoriesModel.SetSortColumnId (0, Gtk.SortType.Ascending);
+			Categories = new ObservableCollection<ICategory> ();
+			var cvCategories = new CollectionView<ICategory> (Categories);
+			cvCategories.SortDescriptions.Add (new SortDescription ());
+			SortedCategories = cvCategories;
 		}
 		
 		#region Public Properties
@@ -69,9 +65,9 @@ namespace Tasque.Backends.Dummy
 		/// <value>
 		/// This returns all the task lists (categories) that exist.
 		/// </value>
-		public Gtk.TreeModel Categories {
-			get { return sortedCategoriesModel; }
-		}
+		public IEnumerable SortedCategories { get; private set; }
+		
+		public ObservableCollection<ICategory> Categories { get; private set; }
 		
 		/// <value>
 		/// Indication that the dummy backend is configured
@@ -116,29 +112,23 @@ namespace Tasque.Backends.Dummy
 		
 		public void Initialize ()
 		{
-			Gtk.TreeIter iter;
-			
 			//
 			// Add in the "All" Category
 			//
-			AllCategory allCategory = new Tasque.AllCategory ();
-			iter = categoryListStore.Append ();
-			categoryListStore.SetValue (iter, 0, allCategory);
+			AllCategory allCategory = new AllCategory ();
+			Categories.Add (allCategory);
 			
 			//
 			// Add in some fake categories
 			//
 			homeCategory = new DummyCategory ("Home");
-			iter = categoryListStore.Append ();
-			categoryListStore.SetValue (iter, 0, homeCategory);
+			Categories.Add (homeCategory);
 			
 			workCategory = new DummyCategory ("Work");
-			iter = categoryListStore.Append ();
-			categoryListStore.SetValue (iter, 0, workCategory);
+			Categories.Add (workCategory);
 			
 			projectsCategory = new DummyCategory ("Projects");
-			iter = categoryListStore.Append ();
-			categoryListStore.SetValue (iter, 0, projectsCategory);
+			Categories.Add (projectsCategory);
 			
 			//
 			// Add in some fake tasks
@@ -240,25 +230,6 @@ namespace Tasque.Backends.Dummy
 		#endregion // Public Methods
 		
 		#region Private Methods
-
-		static int CompareCategorySortFunc (Gtk.TreeModel model,
-											Gtk.TreeIter a,
-											Gtk.TreeIter b)
-		{
-			ICategory categoryA = model.GetValue (a, 0) as ICategory;
-			ICategory categoryB = model.GetValue (b, 0) as ICategory;
-			
-			if (categoryA == null || categoryB == null)
-				return 0;
-			
-			if (categoryA is Tasque.AllCategory)
-				return -1;
-			else if (categoryB is Tasque.AllCategory)
-				return 1;
-			
-			return (categoryA.Name.CompareTo (categoryB.Name));
-		}
-
 		public void UpdateTask (DummyTask task)
 		{
 			if (!Tasks.Contains (task))

src/tasque/Backends/Dummy/DummyCategory.cs
@@ -33,5 +33,19 @@ namespace Tasque.Backends.Dummy
 				return false;
 		}
 		
+		public int CompareTo (ICategory other)
+		{
+			if (other == null)
+				return -1;
+			
+			if (other is AllCategory)
+				return 1;
+			
+			return name.CompareTo (other.Name);
+		}
+
+		#region INotifyPropertyChanged implementation
+		public event PropertyChangedEventHandler PropertyChanged;
+		#endregion
 	}
 }

src/tasque/CompletedTaskGroup.cs
@@ -114,21 +114,10 @@ namespace Tasque
 		{
 			ICategory foundCategory = null;
 			
-			string cat = Application.Preferences.Get (
-							Preferences.SelectedCategoryKey);
+			string cat = Application.Preferences.Get (Preferences.SelectedCategoryKey);
 			if (cat != null) {
-				TreeIter iter;
-				TreeModel model = Application.Backend.Categories;
-				
-				if (model.GetIterFirst (out iter)) {
-					do {
-						ICategory category = model.GetValue (iter, 0) as ICategory;
-						if (category.Name.CompareTo (cat) == 0) {
-							foundCategory = category;
-							break;
-						}
-					} while (model.IterNext (ref iter));
-				}
+				var categories = Application.Backend.Categories;
+				foundCategory = categories.SingleOrDefault (c => c.Name == cat);
 			}
 			
 			return foundCategory;

src/tasque/PreferencesDialog.cs
@@ -31,6 +31,7 @@ using System.Collections.Generic;
 using Gtk;
 using Mono.Unix;
 using Tasque.Backends;
+using CollectionTransforms;
 
 namespace Tasque
 {
@@ -50,7 +51,7 @@ namespace Tasque
 		Dictionary<int, IBackend> backendComboMap; // track backends
 		int 					selectedBackend;
 		Gtk.CheckButton			showCompletedTasksCheckButton;
-		Gtk.TreeModelFilter		filteredCategories;
+		CollectionView<ICategory> filteredCategories;
 		List<string>			categoriesToHide;
 		Gtk.TreeView			categoriesTree;
 
@@ -593,36 +594,16 @@ namespace Tasque
 			}
 			
 			IBackend backend = backendComboMap [selectedBackend];
-			filteredCategories = new TreeModelFilter (backend.Categories, null);
-			filteredCategories.VisibleFunc = FilterFunc;
-			categoriesTree.Model = filteredCategories;
+			filteredCategories = new CollectionView<ICategory> (backend.SortedCategories);
+			// Filter out the AllCategory
+			filteredCategories.Filter = c => c != null && !(c is AllCategory);
+			var adapter = new TreeModelListAdapter<ICategory> (filteredCategories);
+			categoriesTree.Model = adapter;
 		}
 		
 		void OnShown (object sender, EventArgs args)
 		{
 			RebuildCategoryTree ();
-		}
-		
-		/// <summary>
-		/// Filter out the AllCategory
-		/// </summary>
-		/// <param name="model">
-		/// A <see cref="Gtk.TreeModel"/>
-		/// </param>
-		/// <param name="iter">
-		/// A <see cref="Gtk.TreeIter"/>
-		/// </param>
-		/// <returns>
-		/// A <see cref="System.Boolean"/>
-		/// </returns>
-		protected bool FilterFunc (Gtk.TreeModel model,
-										   Gtk.TreeIter iter)
-		{
-			ICategory category = model.GetValue (iter, 0) as ICategory;
-			if (category == null || category is AllCategory)
-				return false;
-			
-			return true;
 		}
 	}
 }

commit a22f946146867337e5c243747ccdbebc15b37829

Fix a bug introduced by 236ee81e81f013fd89972d4e6e5ad58bdc45ab08.

src/tasque/RemoteControl.cs
@@ -267,7 +267,13 @@ namespace Tasque
 				return false;
 			
 			var categories = Application.Backend.Categories;
-			return categories.Contains (task.Category);
+			var cat = categories.SingleOrDefault (c => c.Name == categoryName);
+			if (cat != null) {
+				task.Category = cat;
+				return true;
+			}
+
+			return false;
 		}
 		
 		/// <summary>