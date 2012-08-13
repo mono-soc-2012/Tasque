34ba3bb66ee68c19dd1c352fb29a709a51e7fff5
Antonius Riha <antoniusriha@gmail.com>
2012-08-13 16:39:25 +0200

    Simplify xbuild conf
    * Set OutputPath to "bin".
    * Remove superfluous build configuration groups.

--- src/tasque/Program.cs
+++ src/tasque/Program.cs
@@ -39,7 +39,7 @@ namespace Tasque
 			NativeApplication app;
 #if OSX
 			app = new OSXApplication ();
-#elif WIN32
+#elif GTKWIN
 			app = new GtkWinApplication ();
 #else
 			app = new GtkLinuxApplication ();
