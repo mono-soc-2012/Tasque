e9d947da0b229d032c8d4055a57cd837775bfaad
Antonius Riha <antoniusriha@gmail.com>
2012-08-15 08:38:48 +0200

    Added a common startup project "tasque"

---
+++ src/tasque/Program.cs
@@ -0,0 +1,71 @@
+// 
+// Program.cs
+//  
+// Author:
+//       Antonius Riha <antoniusriha@gmail.com>
+// 
+// Copyright (c) 2012 Antonius Riha
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Diagnostics;
+using Tasque.UIModel.Legacy;
+
+namespace Tasque
+{
+	class Program
+	{
+		static NativeApplication CreateApplication ()
+		{
+			NativeApplication app;
+#if OSX
+			app = new OSXApplication ();
+#else
+			app = new GtkApplication ();
+#endif
+			return app;
+		}
+		
+		static void Main (string[] args)
+		{
+			try {
+				lock (lockObject) {
+					if (application != null)
+						return;
+					
+					application = CreateApplication ();
+				}
+				
+				application.Initialize (args);
+				application.StartMainLoop ();
+			} catch (Exception e) {
+				Debug.WriteLine ("Exception is: {0}", e);
+				application.Exit (-1);
+			} finally {
+				lock (lockObject) {
+					if (application != null)
+						application.Dispose ();
+				}
+			}
+		}
+		
+		static NativeApplication application;
+		static object lockObject = new object ();
+	}
+}
