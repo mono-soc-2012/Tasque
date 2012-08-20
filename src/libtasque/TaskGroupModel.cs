48b8792bb5fa536337e9b52bcde0ac7d1b263723
Antonius Riha <antoniusriha@gmail.com>
2012-08-20 10:11:25 +0200

    [OMG] 4 lettes make all the difference
    When implementing GroupBy support on _List_CollectionView, I also moved sort
    support from CollectionView to ListCollectionView. Alas, I forgot to make
    TaskGroupModel inherit from _List_CollectionView instead of CollectionView. Hence
    the task list sort order wouldn't update id e.g. I changed the due date of a task.

--- src/libtasque/TaskGroupModel.cs
+++ src/libtasque/TaskGroupModel.cs
@@ -29,7 +29,7 @@ using System.Collections.Generic;
 
 namespace Tasque
 {
-	public class TaskGroupModel : CollectionView<Task>
+	public class TaskGroupModel : ListCollectionView<Task>
 	{
 		public bool ShowCompletedTasks {
 			get { return showCompletedTasks; }
